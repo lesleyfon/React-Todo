211bf3b4642d517614faf8986ca29c3c700451f5
lesley <lesleybanadzem2017@gmail.com>
2019-10-03 13:23:53 -0700

    Dummy data for todo

---
+++ src/Data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        todo : 'Go Home',
+        completed : false,
+        index : Date.now()
+    },
+    {
+        todo : 'Call Doctor',
+        completed : false,
+        index : Date.now()
+    },
+    {
+        todo : 'Get groceries',
+        completed : false,
+        index : Date.now()
+    },
+    {
+        todo : 'Eat Food',
+        completed : false,
+        index : Date.now()
+    }
+]
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TodoForm(props) {
+    return (
+        <>
+            <input />
+        </>
+    )
+}
+
+export default TodoForm
